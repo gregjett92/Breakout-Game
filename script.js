302d402a475dea23417a95fb799d88bae94a3364
Greg Jett <greg.jett25@gmail.com>
2020-09-16 15:17:12 -0500

    added functionality to paddle in js

--- script.js
+++ script.js
@@ -87,13 +87,14 @@ function drawBricks() {
     });
   });
 }
-// Move paddle on canvas
-function movePaddle() {
-  padding.x += paddle.dx;
 
-  // Wall dectection
+// Move padddle on canvas
+function movePaddle() {
+  paddle.x += paddle.dx;
+
+  //Wall detection
   if (paddle.x + paddle.w > canvas.width) {
-    padding.x = canvas.width = paddle.w;
+    paddle.x = canvas.width - paddle.w;
   }
 
   if (paddle.x < 0) {
@@ -101,8 +102,22 @@ function movePaddle() {
   }
 }
 
+// Move ball on canvas
+function moveBall() {
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  //    Wall collision (x)
+  if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+    ball.dx *= -1;
+  }
+}
+
 // Draw everything
 function draw() {
+  //Clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   drawBall();
   drawPaddle();
   drawScore();
@@ -112,6 +127,7 @@ function draw() {
 // Update canvas drawing and animation
 function update() {
   movePaddle();
+  moveBall();
 
   // Draw everything
   draw();
@@ -120,6 +136,29 @@ function update() {
 }
 
 update();
+// keydown event
+function keyDown(e) {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    paddle.dx = paddle.speed;
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    paddle.dx = -paddle.speed;
+  }
+}
+
+function keyUp(e) {
+  if (
+    e.key === "Right" ||
+    e.key === "ArrowRight" ||
+    e.key === "Left" ||
+    e.key === "ArrowLeft"
+  ) {
+    padding.dx = 0;
+  }
+}
+
+// Keyboard event handlers
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
 
 // Rules and close event handler
 
